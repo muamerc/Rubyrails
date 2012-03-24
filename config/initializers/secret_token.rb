e1d2a41bda73a6c5da5941a00ecdbe7f75a91947
Muamer cesko <muamer.cesko@gmail.com>
2012-03-24 13:27:43 +0100

    test aplikacija

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PizzaOrdering::Application.config.secret_token = 'a3fed0bdbc4f1f784d323e9c7eba501dc140c8f727c7f0253fbf1b40e9b0ec3090ec57e94395c040e68592c4c1b965d2b070cbbcaedc210856165d688d42e8e9'
